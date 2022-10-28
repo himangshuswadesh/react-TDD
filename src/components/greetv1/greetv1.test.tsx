e1924e3d51f67ece5c36e34c370a5b9fb547cb20
Himangshu Mondal <himangshumondal.swadesh@gmail.com>
2022-10-28 12:19:57 +0530

    test: group greetv1 tests

--- src/components/greetv1/greetv1.test.tsx
+++ src/components/greetv1/greetv1.test.tsx
@@ -6,14 +6,16 @@
 import { render, screen } from '@testing-library/react';
 import Greetv1 from './greetv1.component';
 
-test('GreetV1 should render correctly', () => {
-	render(<Greetv1 />);
-	const textElememt = screen.getByText(/hello/i);
-	expect(textElememt).toBeInTheDocument();
-});
+describe('Greetv1', () => {
+	test('should render correctly', () => {
+		render(<Greetv1 />);
+		const textElememt = screen.getByText(/hello/i);
+		expect(textElememt).toBeInTheDocument();
+	});
 
-test('GreetV1 should render correctly followed by the name', () => {
-	render(<Greetv1 name='John Doe' />);
-	const textElememt = screen.getByText(/hello\sjohn doe/i);
-	expect(textElememt).toBeInTheDocument();
+	test('should render correctly followed by the name', () => {
+		render(<Greetv1 name='John Doe' />);
+		const textElememt = screen.getByText(/hello\sjohn doe/i);
+		expect(textElememt).toBeInTheDocument();
+	});
 });
