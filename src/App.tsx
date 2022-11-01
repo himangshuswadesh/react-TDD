539b20a789982bc97d93569f93eb6f41a9b6da98
Himangshu Mondal <himangshumondal.swadesh@gmail.com>
2022-11-01 19:10:02 +0530

    ui: render skills on app

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,11 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Skills from './components/skills';
 
+const skills = ['HTML', 'CSS', 'JS'];
 function App() {
 	return (
 		<div className='App'>
-			<header className='App-header'>
-				<img src={logo} className='App-logo' alt='logo' />
-				<p>
-					Edit <code>src/App.tsx</code> and save to reload.
-				</p>
-				<a
-					className='App-link'
-					href='https://reactjs.org'
-					target='_blank'
-					rel='noopener noreferrer'>
-					Learn React
-				</a>
-			</header>
+			<Skills skills={skills} />
 		</div>
 	);
 }
